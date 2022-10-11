7befedbc9b4dbc074e6a0a7356e384074b1afc67
Lucas Otávio Leme Silva <49615892+lucaspvpa@users.noreply.github.com>
2022-10-11 01:57:39 -0300

    Busca na Api completa

--- src/API.jsx
+++ src/API.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
 
 function API() {
+    const placeholder = "/placeholder.png";
     const [busca, setBusca] = useState("");
     const [imagemURL, setImagemURL] = useState("");
+    const [erro, setErro] = useState("");
+    return (<div className="busca">
+        <div className="opcoes"><p>biryani</p> <p>burger</p> <p>butter-chicken</p> <p>dessert</p> <p>dosa</p> <p>idly</p> <p>pasta</p> <p>pizza</p> <p>rice</p> </div>
+        <div className="campos-busca">
+            <input placeholder="Buscar imagem de comida" type="text" value={busca} onChange={(e) => setBusca(e.target.value)} />
 
-    return (<>
-        <input type="text" value={busca} onChange={(e) => setBusca(e.target.value)} />
-        <button type="button" onClick={() => {
-            fetch(`https://foodish-api.herokuapp.com/api/images/${busca}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
+            <button type="button" onClick={() => {
+                if (busca.length < 3) {
+                    setErro("Digite pelo menos 3 caracteres");
+                    return
                 }
-            }).then(response => response.json()).then(data => {
-                console.log(data)
-                setImagemURL(data.image)
-            })
-        }}>Buscar</button>
-        <img src={imagemURL} alt="Imagem" />
-    </>)
+                fetch(`https://foodish-api.herokuapp.com/api/images/${busca}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }).then(response => response.json()).then(data => {
+                    if (data.error) {
+                        setErro(data.error);
+                        return
+                    }
+                    if (data.image) {
+                        setImagemURL(data.image)
+                        setErro("")
+                        return
+                    }
+                    setErro("Erro desconhecido");
+                })
+            }}>Buscar</button>
+        </div>
+        {erro !== "" && <p className="erro-busca">{erro}</p>}
+        <div className="imagem-container">
+            <img className="imagem-busca" src={imagemURL || placeholder} alt="Imagem" />
+        </div>
+    </div >)
 }
 export default API;
